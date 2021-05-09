1466993e5648f3d7a65b64f7cedbe2cfe81b5ecc
Bothdream <1368463814@qq.com>
2021-05-09 12:27:31 +0800

    update 记住密码的功能

--- src/main/java/com/zte/auth/config/SecurityConfig.java
+++ src/main/java/com/zte/auth/config/SecurityConfig.java
@@ -13,8 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,26 +34,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private CustomUserDetailService customUserDetailService;
 
+    @Resource(name = "primaryDataSource")
+    private DataSource dataSource;
+
+    @Resource
+    private PersistentTokenRepository persistentTokenRepository;
+
     @Override
     public void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
-                .formLogin()
-                .loginPage("/login.html")// 一旦用户的请求没有权限就跳转到该登录页面
-                .loginProcessingUrl("/login") // 登录表单中action的地址，即处理认证请求的路径
-                .passwordParameter("password") // form表单密码输入框的参数名
-                .usernameParameter("username") // form表单用户名输入框的参数名
-                .successHandler(loginSuccessHandler) // 自定义登录成功的处理逻辑
-                .failureHandler(loginFailHandler)//自定义登录失败的处理逻辑
-                .and()
-                .authorizeRequests()
-                    .antMatchers("/login.html","/login").permitAll()
-                .anyRequest().access("@customRbacService.hasPermission(request,authentication)")// 动态加载资源鉴权规则
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                .sessionFixation().migrateSession()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(false)
-                .expiredSessionStrategy(customExpiredSessionStrategy); //sesscion超时的处理策略
+        http
+                .rememberMe() //记住密码功能配置
+                    .rememberMeParameter("remember-me")
+                    .rememberMeCookieName("cccxx")
+                    .tokenValiditySeconds(2*24*60*60)
+                    .tokenRepository(persistentTokenRepository)
+                .and() // csrf 配置
+                    .csrf()
+                    .disable()
+                .formLogin() // 表单登录配置
+                    .loginPage("/login.html")// 一旦用户的请求没有权限就跳转到该登录页面
+                    .loginProcessingUrl("/login") // 登录表单中action的地址，即处理认证请求的路径
+                    .passwordParameter("password") // form表单密码输入框的参数名
+                    .usernameParameter("username") // form表单用户名输入框的参数名
+                    .successHandler(loginSuccessHandler) // 自定义登录成功的处理逻辑
+                    .failureHandler(loginFailHandler)//自定义登录失败的处理逻辑
+                .and() // 权限过滤配置
+                    .authorizeRequests()
+                    .antMatchers("/login.html","/login")
+                    .permitAll()
+                    .anyRequest()
+                    .access("@customRbacService.hasPermission(request,authentication)")// 动态加载资源鉴权规则
+                .and() // session 管理配置
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .sessionFixation().migrateSession()
+                    .maximumSessions(1)
+                    .maxSessionsPreventsLogin(false)
+                    .expiredSessionStrategy(customExpiredSessionStrategy); //sesscion超时的处理策略
     }
 
     @Override
@@ -72,5 +92,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity webSecurity) {
         webSecurity.ignoring().antMatchers("/css/**","/fonts/**","/img/**","/js/**");
+    }
+
+    // 将记住密码的token持久化到数据库
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        //要创建登录信息表：
+        // create table persistent_logins (username varchar(64) not null, series varchar(64) primary key, token varchar(64) not null, last_used timestamp not null)
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
     }
 }
